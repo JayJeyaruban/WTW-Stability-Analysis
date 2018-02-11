3e5899231b36a2ede5a5c1f6327195f04f67778e
Jay Jeyaruban <jay.jeyaruban.15@ucl.ac.uk>
2018-02-11 19:36:04 +0000

    Swapped to the use of all for partner param
    Successfully able to query in sequence for all files without error

--- Collector.py
+++ Collector.py
@@ -20,10 +20,12 @@ MAX_RETURN = '50000'
 TYPE = 'C'  # C for commodities, S for services
 CC = 'AG2'  # See API docs (https://comtrade.un.org/data/doc/api/#DataRequests) for full info
 FREQ = 'Y'  # M for monthly, Y for yearly
-PX = 'HS'   # See API docs (https://comtrade.un.org/data/doc/api/#DataRequests) for full info
+PX = 'HS'  # See API docs (https://comtrade.un.org/data/doc/api/#DataRequests) for full info
 FMT = 'csv'  # csv or json
 YEARS = range(2000, 2017)
-SKIP_ID = []
+SKIP_ID = [4, 8, 12, 20, 24, 28, 31, 32, 36, 40, 44, 48, 50, 51, 52, 56, 58, 60, 64, 68, 84, 112, 204, 533, 535, 660]
+
+EMPTY_RESPONSE = 'No data matches your query or your query is too complex. Request JSON or XML format for more information.'
 
 
 class ReqMaker:
@@ -43,23 +45,29 @@ class ReqMaker:
             print 'Waiting ', towait / 1000, ' seconds'
             time.sleep(towait / 1000)
 
-        print "Making request"
-        r = requests.get(apiurl, params=parameters)
-        print 'Url: ', r.url
+        done = False
 
-        if r.ok:
-            print "Response OK"
-            print r.status_code
-            self.last_call = self.__gettime()
+        while not done:
+            print "Making request"
+            r = requests.get(apiurl, params=parameters)
+            print 'Url: ', r.url
 
-        elif r.status_code == 409:
-            print 'Calls too frequent, waiting...'
-            time.sleep(5)
+            if r.ok:
+                print "Response OK"
+                print r.status_code
+                self.last_call = self.__gettime()
+                done = True
 
-        else:
-            print "Response not OK ", r.status_code
-            print "Failed call: ", r.url
-            print 'Dump: ', r.content
+            elif r.status_code == 409:
+                print 'Calls too frequent, waiting...'
+                time.sleep(5)
+                continue
+
+            else:
+                print "Response not OK ", r.status_code
+                print "Failed call: ", r.url
+                print 'Dump: ', r.content
+                done = True
 
         return r
 
@@ -68,57 +76,43 @@ def run():
     traders = load_traders()
     # print ids
 
-    partners = load_partners()
-    # print partners
-
     reqmaker = ReqMaker()
     for trader in traders:
         if int(trader['id']) in SKIP_ID:
+            print trader['text'], 'in skip list'
             continue
         # if trader['id'] != '826':
         #     continue
 
-        savedata(makefilenamefromtrader(trader), getdata(reqmaker, trader, partners))
+        savedata(makefilenamefromtrader(trader), getdata(reqmaker, trader))
 
 
 def makefilenamefromtrader(trader):
     return trader['id'] + '-' + trader['text'] + '.csv'
 
 
-def getdata(reqmaker, trader, partners):
+def getdata(reqmaker, trader):
     first = True
+    results = ''
     for year in YEARS:
-        # og_partners = partners.remove(int(trader['id']))
-        og_partners = partners.remove(int(trader['id']))
-        query_partners = og_partners[:PARTNER_QUERY_BLOCK]
-        og_partners = og_partners[PARTNER_QUERY_BLOCK:]
+        payload = makepayload(trader, 'all', year)
+        # print payload
 
-        results = ''
-        while len(og_partners) > 0 | len(query_partners) > 0:
-            fakepartners = [4, 8]
-            # print og_partners
-            payload = makepayload(trader, query_partners, year)
-            # print payload
+        r = reqmaker.makereq(api_url, payload)
+        if r.ok:
+            print 'Successful query; code: ', r.status_code
 
-            r = reqmaker.makereq(api_url, payload)
-            if r.ok:
-                print 'Successful query; code: ', r.status_code
-                query_partners = og_partners[:PARTNER_QUERY_BLOCK]
-                og_partners = og_partners[PARTNER_QUERY_BLOCK:]
-                # print 'Next og: ', og_partners
-                # print 'Next query partners: ', query_partners
+            if EMPTY_RESPONSE in r.content:
+                'Empty response. Skipping...'
+                continue
 
-                if first:
-                    results = results + r.content
-                    first = False
-                else:
-                    results = results + removecsvtitleline(r.content)
-            elif r.status_code == 5004:
-                print 'Too many entries, reducing'
-                og_partners = query_partners[len(query_partners) - QUERY_BLOCK_REDUCE:] + og_partners
-                query_partners = query_partners[:QUERY_BLOCK_REDUCE]
+            if first:
+                results = results + r.content
+                first = False
             else:
-                sys.exit(1)
+                results = results + removecsvtitleline(r.content)
+        else:
+            sys.exit(1)
 
     return results
 
@@ -128,16 +122,16 @@ def removecsvtitleline(csv_string):
     return csv_string[(nli + 1):]
 
 
-def makepayload(trader, partners, year):
-    print 'Making payload for ', trader, 'with year ', year, 'and partners ', partners
-    formatted_partners = ''
-    for partner in partners:
-        if formatted_partners == '':
-            formatted_partners = str(partner)
-        else:
-            formatted_partners = formatted_partners + ',' + str(partner)
+def makepayload(trader, partner, year):
+    print 'Making payload for ', trader, 'with year ', year, 'and partners ', partner
+    # formatted_partners = ''
+    # for partner in partners:
+    #     if formatted_partners == '':
+    #         formatted_partners = str(partner)
+    #     else:
+    #         formatted_partners = formatted_partners + ',' + str(partner)
 
-    return {'r': trader['id'], 'freq': FREQ, 'ps': year, 'px': PX, 'p': formatted_partners, 'cc': CC,
+    return {'r': trader['id'], 'freq': FREQ, 'ps': year, 'px': PX, 'p': partner, 'cc': CC,
             'fmt': FMT, 'max': MAX_RETURN, 'type': TYPE}
 
 
