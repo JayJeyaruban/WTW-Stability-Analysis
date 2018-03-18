8af055b477ac1a28579e62cc3b24e755c88e58bc
Jay Jeyaruban <jay.jeyaruban.15@ucl.ac.uk>
2018-03-18 18:25:51 +0000

    Working deletion sim

--- Analysis.pyx
+++ Analysis.pyx
@@ -1,13 +1,15 @@
-import networkx
-import json
-import pandas
-from pandas.errors import EmptyDataError
-import glob
-import os
-import matplotlib.pyplot as pyplot
-import numpy
-from numpy.linalg import inv
 import copy
+import glob
+import json
+import os
+
+import matplotlib.pyplot as pyplot
+import networkx
+import numpy
+import pandas
+import pyximport; pyximport.install()
+from pandas.errors import EmptyDataError
+
 
 data_dir = 'output/'
 countries_file = 'resources/traders.json'  # File with trader information
@@ -26,57 +28,108 @@ def run():
     partners = load_partners()
     db = setupdb()
 
+    node_deletion_results = []
     for year in YEARS:
-        imports, aggregate, reporters, comm_map = makematrices(db, partners, year)
+        imports, aggregate, reporters, comm_list = makematrices(db, partners, year)
         # print(imports)
         # print(exports)
         agg = matrix_calcs(aggregate)
 
-        node_deletion_sim(agg, imports, reporters)
+        node_deletion_results.extend(node_deletion_sim(year, agg, imports, reporters, comm_list))
+
+    node_deleteion_db = pandas.concat(node_deletion_results)
+    node_deleteion_db.to_pickle("results/node_deletion.p")
 
 
-def node_deletion_sim(aggregate, imports, reporters):
+def node_deletion_sim(year, aggregate, imports, reporters, commodities):
+    results = []
     for i, comm_matrix in enumerate(imports):
+        print("Running sim for comm:", commodities[i])
         # Pre-shock statistics for comm network
         curr_stats = matrix_calcs(comm_matrix)
-        for reporter in list(range(0, len(reporters))):
+        for j, reporter in enumerate(reporters):
+            print("Running for reporter:", reporter)
             # First post-shock statistics for comm network given 'reporter' stops trading
-            shocked_matrix_stats = node_deletion(curr_stats, reporter)
+            shocked_agg_stats, shocked_matrix_stats = node_deletion(aggregate, curr_stats, j)
+            initial_loss = curr_stats['e'] - shocked_matrix_stats['e']
 
-            # Next iteration
+            # Iteration for comm network
             prev_M = copy.deepcopy(shocked_matrix_stats['matrix'])
             prev_E = e_matrix_calc(shocked_matrix_stats['alpha'], shocked_matrix_stats['beta'], prev_M)
             curr_M = iterate_matrix(prev_E, shocked_matrix_stats['m'])
             curr_E = e_matrix_calc(shocked_matrix_stats['alpha'], shocked_matrix_stats['beta'], curr_M)
             iterations = 1
-            while calc_rms(prev_E, curr_E) > 0.01 and iterations < 101:
-                print('Iteration:', iterations, "score:", calc_rms(prev_E, curr_E))
+            comm_rms = calc_rms(prev_E, curr_E)
+            while comm_rms > 0.01 and iterations < 101:
+                # print('Iteration:', iterations, "rms:", comm_rms)
                 iterations += 1
                 prev_E = curr_E
                 curr_M = iterate_matrix(curr_E, shocked_matrix_stats['m'])
                 curr_E = e_matrix_calc(shocked_matrix_stats['alpha'], shocked_matrix_stats['beta'], curr_M)
+                comm_rms = calc_rms(prev_E, curr_E)
+
+            print("Final it", iterations, "final rms;", comm_rms)
+            exp_diff = curr_stats['e'] - curr_E
+            amplification = sum(exp_diff) / sum(initial_loss)
+
+            # Iteration for agg network
+            agg_prev_e = shocked_agg_stats['e']
+            agg_curr_M = iterate_matrix(agg_prev_e, shocked_agg_stats['m'])
+            agg_curr_e = e_matrix_calc(shocked_agg_stats['alpha'], shocked_agg_stats['beta'], agg_curr_M)
+            agg_iterations = 1
+            agg_rms = calc_rms(agg_prev_e, agg_curr_e)
+            while agg_rms > 0.01 and agg_iterations < 1001:
+                # print('Agg Iteration:', agg_iterations, "rms", agg_rms)
+                agg_iterations += 1
+                agg_prev_e = agg_curr_e
+                agg_curr_M = iterate_matrix(agg_curr_e, shocked_agg_stats['m'])
+                agg_curr_e = e_matrix_calc(shocked_agg_stats['alpha'], shocked_agg_stats['beta'], agg_curr_M)
+                agg_rms = calc_rms(agg_prev_e, agg_curr_e)
+
+            print("Final agg it:", agg_iterations, "final rms", agg_rms)
+            agg_exp_diff = aggregate['e'] - agg_curr_e
+            agg_amplification = sum(agg_exp_diff) - sum(initial_loss)
+
+            entry = pandas.DataFrame(
+                {'year': year, 'country': reporter, 'commodity': commodities[i], 'initial_loss': sum(initial_loss),
+                 'comm_iterations': iterations, 'final_comm_loss': sum(exp_diff), 'comm_amplication': amplification,
+                 'agg_iterations': agg_iterations, 'final_agg_loss': agg_exp_diff,
+                 'agg_amplification': agg_amplification})
+
+            results.append(entry)
+
+    return results
 
 
 def iterate_matrix(old_exp, m):
     return numpy.matmul(numpy.diag(old_exp), m)
 
 
-def node_deletion(comm_import_stats, reporter):
+def node_deletion(agg_stats, comm_import_stats, reporter):
     shocked_matrix_stats = copy.deepcopy(comm_import_stats)
+    shocked_agg_stats = copy.deepcopy(agg_stats)
 
-    # Apply shock to each trade reporter has for specific commodity
+    # Apply shock to each trade reporter 'i' has for specific commodity
     for i in range(len(shocked_matrix_stats['matrix'])):
+        shocked_agg_stats['matrix'][reporter, i] -= shocked_matrix_stats['matrix'][reporter, i]
+        shocked_agg_stats['matrix'][i, reporter] -= shocked_matrix_stats['matrix'][i, reporter]
+        shocked_agg_stats['m'][reporter, i] -= shocked_matrix_stats['m'][reporter, i]
+        shocked_agg_stats['m'][i, reporter] -= shocked_matrix_stats['m'][i, reporter]
+
         shocked_matrix_stats['matrix'][reporter, i] = 0
         shocked_matrix_stats['matrix'][i, reporter] = 0
         shocked_matrix_stats['m'][reporter, i] = 0
         shocked_matrix_stats['m'][i, reporter] = 0
 
+    shocked_agg_stats['alpha'][reporter] -= shocked_matrix_stats['alpha'][reporter]
     shocked_matrix_stats['alpha'][reporter] = 0
 
+    shocked_agg_stats['e'] = e_matrix_calc(shocked_agg_stats['alpha'], shocked_agg_stats['beta'],
+                                           shocked_agg_stats['matrix'])
     shocked_matrix_stats['e'] = e_matrix_calc(shocked_matrix_stats['alpha'], shocked_matrix_stats['beta'],
                                               shocked_matrix_stats['matrix'])
 
-    return shocked_matrix_stats
+    return shocked_agg_stats, shocked_matrix_stats
 
 
 def calc_rms(prev_E, curr_E):
@@ -88,14 +141,13 @@ def matrix_calcs(comm_matrix):
     Om = numpy.sum(comm_matrix, axis=0)
     # print(Im)
     # print(Om)
-    alpha = [i / j if j and i >= j else 1 for i, j in zip(Im, Om)]
+    alpha = [j / i if i and i > j else 1 for i, j in zip(Im, Om)]
     beta = [j - i if j > i else 0 for i, j in zip(Im, Om)]
     m = numpy.zeros((len(comm_matrix), len(comm_matrix)))
     for i, x in enumerate(comm_matrix):
         for j, y in enumerate(x):
             m[i, j] = y / Om[i] if Om[i] > 0 else 0
 
-    # TODO Check this formula
     e = e_matrix_calc(alpha, beta, m)
 
     return {"alpha": alpha, "beta": beta, "m": m, "e": e, 'matrix': comm_matrix}
@@ -115,7 +167,7 @@ def makematrices(db, partners, year):
     # print(len(list(map(lambda x: x['id'], traders))))
     commodities = pandas.unique(db.CommodityCode.values.ravel())
     commodities.sort()
-    imports = [numpy.zeros((unique_entries, unique_entries)) for i in commodities]
+    imports = [numpy.zeros((unique_entries, unique_entries)) for _ in commodities]
     aggregate = numpy.zeros((unique_entries, unique_entries))
     # exports = [numpy.zeros((unique_entries, unique_entries)) for i in commodities]
 
@@ -127,13 +179,13 @@ def makematrices(db, partners, year):
         # i is index position in matrix
         results = db[(db['ReporterCode'] == reporter) & (db['Year'] == year)]
         if results.shape[0] == 0:
-            print("No results for code:", reporter)
+            # print("No results for code:", reporter)
             continue
 
         for row in results.itertuples():
             partner_code = getattr(row, 'PartnerCode')
             if partner_code == 0:
-                print("Ignoring world for reporter:", reporter)
+                # print("Ignoring world for reporter:", reporter)
                 continue
             # print(row)
             flow = getattr(row, 'TradeFlowCode')
@@ -144,15 +196,15 @@ def makematrices(db, partners, year):
             comm_index = commodity_to_i[comm_code]
             matrix = imports[comm_index]
             if flow in [IMPORT, REIMPORT]:
-                print("Adding", flow_amount, "to", i, partner_index, "on", comm_index, "in imports")
+                # print("Adding", flow_amount, "to", i, partner_index, "on", comm_index, "in imports")
                 matrix[i, partner_index] = flow_amount
                 aggregate[i, partner_index] = flow_amount
             else:
-                print("Adding", flow_amount, "to", i, partner_index, "on", comm_index, "in exports")
+                # print("Adding", flow_amount, "to", i, partner_index, "on", comm_index, "in exports")
                 matrix[partner_index, i] = flow_amount
                 aggregate[partner_index, i] = flow_amount
 
-    return imports, aggregate, reporters, commodity_to_i
+    return imports, aggregate, reporters, commodities
 
 
 def setupdb():
@@ -192,86 +244,6 @@ def loaddb():
     return pandas.read_pickle("db.p")
 
 
-def setupgraphs(db, countries):
-    # print(db)
-    graphs = []
-    for year in range(2000, 2017):
-        graph = networkx.DiGraph()
-        for country in countries:
-            id = int(country['id'])
-            result = db[(db['ReporterCode'] == id) & (db['Year'] == year)]
-            if result.shape[0] == 0:
-                print("No results")
-                continue
-
-            print(result)
-
-            addtograph(graph, id, result.itertuples())
-
-        graphs.append((year, graph))
-
-    return graphs
-
-
-def addtograph(graph, country_code, trade_data):
-    if not graph.has_node(country_code):
-        graph.add_node(country_code)
-
-    for row in trade_data:
-        # print(row)
-        partner = getattr(row, "PartnerCode")
-        if partner == 0:
-            continue
-
-        if not graph.has_node(partner):
-            graph.add_node(partner)
-
-        flow = getattr(row, 'TradeFlowCode')
-        flow_amount = getattr(row, 'TradeValue')
-        comm_code = getattr(row, 'CommodityCode')
-
-        edge = tradeflowswitch(country_code, partner, flow)
-        if edge == -1:
-            print("Handle invalid flow", flow)
-
-        edge
-        print(edge)
-        if graph.has_edge(*edge):
-            print("To handle")
-
-        graph.add_weighted_edges_from(edge + ({'weight': flow_amount, 'comm': comm_code},))
-
-
-def drawgraphs(graphs):
-    for i, graph in enumerate(graphs):
-        pos = networkx.layout.spring_layout(graph)
-
-        nodesize = [3 + 10 * i for i in range(len(graph))]
-        M = graph.number_of_edges()
-        edge_colors = range(2, M + 2)
-        edge_alphas = [(5 + 1 / M + 4) for i in range(M)]
-
-        nodes = networkx.draw_networkx_nodes(graph, pos, node_size=nodesize, node_color='blue')
-        edges = networkx.draw_networkx_edges(graph, pos, node_size=nodesize, arrowstyle='->', arrowsize=10,
-                                             edge_colors=edge_colors, edge_cmap=pyplot.cm.Blues, width=2)
-
-        for i in range(M):
-            edges[i].set_alpha(edge_alphas[i])
-
-        ax = pyplot.gca()
-        ax.set_axis_off()
-        pyplot.savefig(i + '.png')
-
-
-def tradeflowswitch(country1, country2, flow):
-    return {
-        IMPORT: (country2, country1),
-        EXPORT: (country1, country2),
-        REIMPORT: (country2, country1),
-        REEXPORT: (country1, country2)
-    }.get(flow, -1)
-
-
 def loadcountries():
     with open(countries_file, newline='\n') as file:
         traders = json.load(file)
@@ -280,7 +252,7 @@ def loadcountries():
 
 
 def load_partners():
-    with open(partners_file) as partner_data:
+    with open(partners_file, encoding='utf-8') as partner_data:
         ignore = ['all', '0']
         partners = json.load(partner_data)
 
